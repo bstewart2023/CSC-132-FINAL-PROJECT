957c37f44e69f9e4637afba30b96d81c66d00353
Brianna Stewart <xxbriestewartxx@gmail.com>
2020-03-13 14:25:29 -0500

    added trial.py first draft

---
+++ Trial.py
@@ -0,0 +1,20 @@
+def function1():
+    pass
+
+def function2():
+    pass
+
+def function3():
+    pass
+
+def function4():
+    pass
+
+############
+### MAIN ###
+############
+
+function1()
+function2()
+function3()
+function4()
