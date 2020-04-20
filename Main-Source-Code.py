3584a1eb21549261f8a12583a05fe831bbe45a48
Corey Belk <coreyleebelk@icloud.com>
2020-04-20 15:03:53 -0500

    Altered name of file

---
+++ Main-Source-Code.py
@@ -0,0 +1,33 @@
+#############################################
+########USE#THIS#TEMPLATE#AS#OUTLINE#########
+#############################################
+
+
+# imports any necessary libraries
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.resolution = (2592, 1944)
+camera.start_preview()
+
+sleep(2)
+camera.capture("IMAGES/image.jpg")
+
+
+# defines classes/methods/processes needed for accessing IP camera
+
+
+
+
+# defines classes/methods/processes needed for interpreting/managing motion detection
+
+
+
+
+# defines classes/methods/processes needed for relaying interpreted data to pi/rpi
+
+
+
+
+# displays all final data gathered through intuitive GUI
