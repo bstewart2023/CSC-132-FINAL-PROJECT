ac53b24676ec9d35d7fbf62a8951f605cf7f25d5
Corey Belk <coreyleebelk@icloud.com>
2020-04-22 01:15:15 -0500

    Added Canny Edge Detection + Results

--- Main-Source-Code.py
+++ Main-Source-Code.py
@@ -6,16 +6,35 @@
 # imports any necessary libraries
 from picamera import PiCamera
 from time import sleep
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
+# Captures an image and saves it to a file
 camera = PiCamera()
 camera.resolution = (2592, 1944)
 camera.hflip = True
-camera.start_preview()
+##camera.start_preview()
 # Time taken before image is captured
 sleep(2)
-camera.capture("IMAGES/image1.jpg")
-sleep(2)
-camera.capture("IMAGES/image2.jpg")
+camera.capture("IMAGES/image.jpg")
+##sleep(2)
+##camera.capture("IMAGES/image6.jpg")
+
+# OpenCV image detection that turns image to specified shade
+# then maps and plots edges found in image
+img = cv2.imread('IMAGES/image.jpg', 100)
+edges = cv2.Canny(img, 160, 50, 3)
+
+##edges = cv2.Canny(img, 60, 60, 3)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
 
 # defines classes/methods/processes needed for accessing IP camera
 
