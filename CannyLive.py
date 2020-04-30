f88ccba8d96c639ab69dfcb8405f0ec325c37f12
GBJones2019 <GBJones2019@users.noreply.github.com>
2020-04-30 18:22:32 -0500

    Smoothed the Canny Edges, addes a possible technique to determine a car.

--- CannyLive.py
+++ CannyLive.py
@@ -9,37 +9,37 @@ import os
 def main():
 
     # Uses primary webcam
-    capWebcam = cv2.VideoCapture(0)
+    capWebcam = cv2.VideoCapture("IMAGES/nocar.jpg")
 
 ##    print ("default resolution = " + str(capWebcam.get(cv2.CAP_PROP_FRAME_WIDTH)) + "x" + str(capWebcam.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
-    # Changes resolution for smooth results
-    capWebcam.set(cv2.CAP_PROP_FRAME_WIDTH, 240.0)
-    capWebcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240.0)
+    # Changes resolution for smooth results; EDIT: made it smoother :P
+    capWebcam.set(3, 640.0)
+    capWebcam.set(4, 480.0)
 
 ##    print ("updated resolution = " + str(capWebcam.get(cv2.CAP_PROP_FRAME_WIDTH)) + "x" + str(capWebcam.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
-    # Checks for connection to webcam
-    if capWebcam.isOpened() == False:
-        print ("error: capWebcam not accessed successfully\n\n")
-        os.system("pause")
-        return
-
-    # Closes live feed windows with esc key
-    while cv2.waitKey(1) != 27 and capWebcam.isOpened():
-        blnFrameReadSuccessfully, imgOriginal = capWebcam.read()
-
-        # Checks if frames were read correctly through webcam
-        if not blnFrameReadSuccessfully or imgOriginal is None:
-            print ("error: frame not read from webcam\n")
-            os.system("pause")
-            break
+##    # Checks for connection to webcam
+##    if capWebcam.isOpened() == False:
+##        print ("error: capWebcam not accessed successfully\n\n")
+##        os.system("pause")
+##        return
+##
+##    # Closes live feed windows with esc key
+##    while cv2.waitKey(1) != 27 and capWebcam.isOpened():
+##        blnFrameReadSuccessfully, imgOriginal = capWebcam.read()
+##
+##        # Checks if frames were read correctly through webcam
+##        if not blnFrameReadSuccessfully or imgOriginal is None:
+##            print ("error: frame not read from webcam\n")
+##            os.system("pause")
+##            break
 
         # Takes image and turns it to grayscale
-        imgGrayscale = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
-
+        imgBlurred = cv2.GaussianBlur(imgOriginal, (7, 7), 1.41)
+        
         # Takes grayscaled image and blurs it
-        imgBlurred = cv2.GaussianBlur(imgGrayscale, (1, 1), 0)
+        imgGrayscale = cv2.cvtColor(imgBlurred, cv2.COLOR_BGR2GRAY)
 
         # Takes blurred image and detects edges using canny
         imgCanny = cv2.Canny(imgBlurred, 100, 100)
@@ -52,10 +52,27 @@ def main():
         cv2.imshow("imgOriginal", imgOriginal)
         cv2.imshow("imgCanny", imgCanny)
 
+        # Gets totale number of pixels, white pixesls, and black pixels from the Canny video feed
+        pixels = imgCanny.size
+        w_pixels = cv2.countNonZero(imgCanny)
+        b_pixels = (pixels - w_pixels)
+
+        if (w_pixels / pixels) < 0.0105 and (w_pixels / pixels) > 0.01:
+            print ("[OPEN]")
+        else:
+            print ("[TAKEN]")
+            
+
     cv2.destroyAllWindows()
+
+    print (pixels)
+    print(w_pixels)
+    print (b_pixels)
 
     return
 
+
+    
 ###################################################################################################
 ##if __name__ == "__main__":
 main()
