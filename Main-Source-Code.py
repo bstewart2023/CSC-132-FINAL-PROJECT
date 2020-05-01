d56c0fd84d73061587abb40b6f434a9b83da6e8b
Corey Belk <coreyleebelk@icloud.com>
2020-04-30 22:59:38 -0500

    Updated

--- Main-Source-Code.py
+++ Main-Source-Code.py
@@ -4,25 +4,25 @@ from __future__ import division
 #############################################
 
 ### imports any necessary libraries
-##from picamera import PiCamera
-from time import sleep
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-
-img = cv2.imread("IMAGES/image10.jpg", cv2.IMREAD_GRAYSCALE)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-blur = cv2.GaussianBlur(img, (3,3), 0)
-edges = cv2.Canny(blur,100,200,7)
-
-titles = ['image','Canny']
-images = [img, edges]
-for i in range(2):
-    plt.subplot(1, 2, i+1), plt.imshow(images[i], 'gray')
-    plt.title(titles[i])
-    plt.xticks([]),plt.yticks([])
-
-plt.show()
+from picamera import PiCamera
+##from time import sleep
+##import cv2
+##import numpy as np
+##from matplotlib import pyplot as plt
+##
+##img = cv2.imread("IMAGES/image10.jpg", cv2.IMREAD_GRAYSCALE)
+##img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+##blur = cv2.GaussianBlur(img, (3,3), 0)
+##edges = cv2.Canny(blur,100,200,7)
+##
+##titles = ['image','Canny']
+##images = [img, edges]
+##for i in range(2):
+##    plt.subplot(1, 2, i+1), plt.imshow(images[i], 'gray')
+##    plt.title(titles[i])
+##    plt.xticks([]),plt.yticks([])
+##
+##plt.show()
 
 ### Captures an image and saves it to a file
 ##camera = PiCamera()
